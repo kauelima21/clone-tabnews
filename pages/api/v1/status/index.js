d389dd30e2a1e76ee29df591d7ca29a6993a6b63
kauelima21 <kaueslim@outlook.com>
2023-12-23 17:43:37 -0300

    add `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,13 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
   response.status(200).json(
     {
       status: "Tudo legal.",
     }
   );
 }
+
+export default status;
