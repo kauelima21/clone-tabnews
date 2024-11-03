dec34272251cc3f3a35c52fddcd6e10860550e84
kauelima21 <kaueslim@outlook.com>
2024-11-03 10:21:18 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,6 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   response.status(200).send({ chave: "valor" });
 }
 
