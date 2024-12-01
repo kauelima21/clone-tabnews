788d8f30b9e44cade78340c2edfb03d684230e98
kauelima21 <kaueslim@outlook.com>
2024-12-01 11:12:04 -0300

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,13 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${(request, method)}" not allowed`,
+    });
+  }
+
   let dbClient;
 
   try {
@@ -35,8 +42,6 @@ export default async function migrations(request, response) {
 
       return response.status(statusCode).json(migratedMigrations);
     }
-
-    response.status(405).end();
   } catch (error) {
     console.log(error);
     throw error;
