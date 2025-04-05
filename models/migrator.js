0b76408af3e9d4fcdaf8f23192bd75c74bd8db74
kauelima21 <kaueslim@outlook.com>
2025-04-05 10:54:13 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,49 @@
+import { resolve } from "node:path";
+import database from "../infra/database";
+import migrationRunner from "node-pg-migrate";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    return await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    return await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+}
+
+export default migrator;
